f3c373fdf4aa1ec00d987db4ab7c7da4f2afa453
clawud@proton.me <clawud@proton.me>
2023-10-12 14:28:32 +0530

    Updated

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(session({
 const connection = mysql.createConnection({
 	host: "localhost",
 	user: "root",
-	password: "Ajinkya*123",
+	password: "Megh*2302",
 	database: "chatbot",
 });
 
