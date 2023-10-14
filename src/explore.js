9614d17c4da99093bbba0da29a4b83e976a37ac7
clawud@proton.me <clawud@proton.me>
2023-10-14 11:30:14 +0530

    Explore page

--- src/explore.js
+++ src/explore.js
@@ -1,25 +1,31 @@
-$(document).ready(function(){ 
- 
-    $(".primaryContained").on('click', function(){
-    $(".comment").addClass("commentClicked");
-   });//end click
-   $("textarea").on('keyup.enter', function(){
-    $(".comment").addClass("commentClicked");
-   });//end keyup
-   });//End Function
-   
-   new Vue({
-    el: "#app",
-    data:{
-       title: 'Add a comment',
-      newItem: '',
-      item: [],
-    },
-    methods:{
-      addItem  (){
-      this.item.push(this.newItem);
-        this.newItem = "";
-      }
-   }
-   
-   });
+function showCityPictures(city) {
+  const imageContainer = document.getElementById('imageContainer');
+  imageContainer.innerHTML = ''; // Clear previous images
+
+  const cities = {
+    'New York': [
+      'megh.jpg',
+      //'new-york-image2.jpg'
+    ],
+    'London': [
+      'london-image1.jpg',
+      'london-image2.jpg'
+    ],
+    'Paris': [
+      'paris-image1.jpg',
+      'paris-image2.jpg'
+    ]
+  };
+
+  const cityImages = cities[city];
+
+  if (cityImages) {
+    for (const imageSrc of cityImages) {
+      const imgElement = document.createElement('img');
+      imgElement.src = 'assets/img/' + imageSrc; // Assuming images are in a folder named "images"
+      imageContainer.appendChild(imgElement);
+    }
+  } else {
+    imageContainer.innerHTML = 'No images available for this city.';
+  }
+}
